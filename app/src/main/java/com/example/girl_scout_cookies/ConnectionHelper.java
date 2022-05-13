d5e362e5febc74aafde82a612a0a0d532040978b
tanjamu <98948585+tanjamu@users.noreply.github.com>
2022-05-13 12:54:47 +0200

    more code to connect to localhost Bas

--- app/src/main/java/com/example/girl_scout_cookies/ConnectionHelper.java
+++ app/src/main/java/com/example/girl_scout_cookies/ConnectionHelper.java
@@ -6,27 +6,30 @@ import android.util.Log;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
 
 public class ConnectionHelper {
     Connection con;
     String uname, pass, ip, port, database;
+    String ConnectionURL = null;
 
     @SuppressLint("NewApi")
     public Connection connectionClass() {
-        ip = "172.1.1.0";
-        database = "id18911941_girlscoutcookies";
-        uname = "id18911941_girlscoutcookiesapp";
-        pass = ")g?u]=U2U+tI<erV";
-        port = "21";
+        ip = "145.116.135.249";
+        database = "Test";
+        uname = "Test";
+        pass = "password";
+        port = "1433";
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         Connection connection = null;
-        String ConnectionURL = null;
+        ConnectionURL = null;
 
         try {
-            Class.forName("net.sourceforge.jtds.jdbc.Driver");
-            ConnectionURL = "jdbc:jtds:sqlserver://"+ip+":"+port+";"+"databasename="+database+";user="+uname+";password="+pass+";";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            ConnectionURL = "jdbc:sqlserver://"+ip+":"+port+";"+"databasename="+database+";user="+uname+";password="+pass+";";
             connection = DriverManager.getConnection(ConnectionURL);
         } catch (Exception ex) {
             Log.e("Error ", ex.getMessage());
--- app/src/main/java/com/example/girl_scout_cookies/ToMap.java
+++ app/src/main/java/com/example/girl_scout_cookies/ToMap.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
 
 public class ToMap extends AppCompatActivity {
     Connection connect;
@@ -21,7 +25,7 @@ public class ToMap extends AppCompatActivity {
         setContentView(R.layout.activity_to_map);
         // Get the Intent that started this activity and extract the string
         android.content.Intent intent = getIntent();
-        String message=getIntent().getStringExtra("Girl-Scout-Cookies.MESSAGE");
+        String message = getIntent().getStringExtra("Girl-Scout-Cookies.MESSAGE");
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
@@ -31,23 +35,54 @@ public class ToMap extends AppCompatActivity {
         TextView tx1 = findViewById(R.id.textView3);
         TextView tx2 = findViewById(R.id.textView4);
 
+
+        ConnectionHelper connectionHelper = new ConnectionHelper();
+//            connect = connectionHelper.connectionClass();
+
         try {
-            ConnectionHelper connectionHelper = new ConnectionHelper();
-            connect = connectionHelper.connectionClass();
+
+            String dbURL = connectionHelper.ConnectionURL;
+            String user = connectionHelper.uname;
+            String pass = connectionHelper.pass;
+            connect = DriverManager.getConnection(dbURL, user, pass);
             if (connect != null) {
-                String query = "SELECT * FROM [tablename] WHERE blablabla";
-                Statement st = connect.createStatement();
-                ResultSet rs = st.executeQuery(query);
-
-                while (rs.next()) {
-                    tx1.setText(rs.getString(1));
-                    tx2.setText(rs.getString(2));
-                }
-            } else {
-                ConnectionResult = "Check Connection";
+                DatabaseMetaData dm = (DatabaseMetaData) connect.getMetaData();
+                tx1.setText(dm.getDriverName());
+//                System.out.println("Driver version: " + dm.getDriverVersion());
+//                System.out.println("Product name: " + dm.getDatabaseProductName());
+//                System.out.println("Product version: " + dm.getDatabaseProductVersion());
             }
-        } catch(Exception ex) {
 
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (connect != null && !connect.isClosed()) {
+                    connect.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
     }
+
+//            if (connect != null) {
+//                String query = "SELECT * FROM test2";
+//                Statement st = connect.createStatement();
+//                ResultSet rs = st.executeQuery(query);
+//
+//                ConnectionResult = "Successfully connected";
+//
+//                while (rs.next()) {
+//                    tx1.setText(rs.getString(1));
+//                    tx2.setText(rs.getString(2));
+//                }
+//            } else {
+//                ConnectionResult = "Check Connection";
+//            }
+//        } catch(Exception ex) {
+//            Log.e("Error ", ex.getMessage());
+//        }
+//        tx2.setText(ConnectionResult);
+//}
 }
