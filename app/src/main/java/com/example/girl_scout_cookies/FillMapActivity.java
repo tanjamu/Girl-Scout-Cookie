a39f250a902810e348ffa9cf0584882f8c00478b
eskildam <eskildam@outlook.com>
2022-05-24 16:22:52 +0200

    made fillmap branch

--- app/src/main/java/com/example/girl_scout_cookies/FillMapActivity.java
+++ app/src/main/java/com/example/girl_scout_cookies/FillMapActivity.java
@@ -2,13 +2,22 @@ package com.example.girl_scout_cookies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class FillMapActivity extends AppCompatActivity {
     Button btnEnterSubmit, btnFinishCancel;
+    Geocoder geocoder;
+    LatLng latLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,8 @@ public class FillMapActivity extends AppCompatActivity {
         btnEnterSubmit.setOnClickListener(this::EnterSubmit);
         btnFinishCancel.setOnClickListener(this::FinishCancel);
 
+        geocoder = new Geocoder(getApplicationContext());
+
         // Get the Intent that started this activity and extract the string
         String message = getIntent().getStringExtra("Girl-Scout-Cookies.MESSAGE");
         // Capture the layout's TextView and set the string as its text
@@ -29,6 +40,13 @@ public class FillMapActivity extends AppCompatActivity {
     }
 
     private void Enter(View view) {
+        List<Address> list = new LinkedList<Address>();
+        try {
+            list = geocoder.getFromLocationName("Huygensgebouw", 1);
+            latLng = new LatLng(list.get(0).getLatitude(), list.get(0).getLongitude());
+        } catch (Exception e) {
+
+        }
 
     }
 
@@ -45,21 +63,25 @@ public class FillMapActivity extends AppCompatActivity {
     }
 
     private void EnterSubmit(View view) {
-        if (btnEnterSubmit.getText().equals(R.string.enter)) {
+        if (btnEnterSubmit.getText().equals(getString(R.string.enter))) {
             Enter(view);
             btnEnterSubmit.setText(R.string.submit);
+            btnFinishCancel.setText(R.string.cancel);
         } else {
             Submit(view);
             btnEnterSubmit.setText(R.string.enter);
+            btnFinishCancel.setText(R.string.finish);
         }
     }
 
     private void FinishCancel(View view) {
-        if (btnEnterSubmit.getText().equals(R.string.finish)) {
+        if (btnEnterSubmit.getText().equals(getString(R.string.finish))) {
             Finish(view);
+            btnEnterSubmit.setText(R.string.submit);
             btnFinishCancel.setText(R.string.cancel);
         } else {
             Cancel(view);
+            btnEnterSubmit.setText(R.string.enter);
             btnFinishCancel.setText(R.string.finish);
         }
     }
