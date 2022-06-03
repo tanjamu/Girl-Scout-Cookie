afd78a35f1586f2fa1d39b003b4cc3b259d003c4
eskildam <eskildam@outlook.com>
2022-06-03 03:21:11 +0200

    cleaned up fillmap
    Created FillMapBackend
    Cleaned up FillMapActivity

--- app/src/main/java/com/example/girl_scout_cookies/FillMapActivity.java
+++ app/src/main/java/com/example/girl_scout_cookies/FillMapActivity.java
@@ -1,18 +1,8 @@
 package com.example.girl_scout_cookies;
 
-import static android.graphics.Color.YELLOW;
-
-import static com.example.girl_scout_cookies.MyColor.*;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentContainerView;
-import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.location.Address;
-import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -20,218 +10,178 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.google.android.gms.common.util.ArrayUtils;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.lang.Object;
-import java.sql.Array;
-import java.sql.Blob;
-import java.sql.CallableStatement;
-import java.sql.Clob;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.NClob;
-import java.sql.PreparedStatement;
-import java.sql.SQLClientInfoException;
-import java.sql.SQLException;
-import java.sql.SQLWarning;
-import java.sql.SQLXML;
-import java.sql.Savepoint;
-import java.sql.Statement;
-import java.sql.Struct;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Properties;
 
 public class FillMapActivity extends AppCompatActivity {
-    int mapID;
-    GoogleMap mMap;
+    FillMapBackend fillMapBackend;
+
+    TextView mapNameTextView;
     TextView errorTextView; // For displaying error messages to the user
     TextView addressTextView; // For entering addresses to add to the map
     Spinner spinner; // For selecting colors
-    ArrayAdapter<CharSequence> arrayAdapter; // Fills the spinner
 
-    Button btnEnterSubmit;
-    Button btnFinishCancel;
+    Button btnEnterSubmitDelete;
+    Button btnCancelSave;
 
     FillMapFragment fragment; // The map
-    Geocoder geocoder; // For transforming address queries into Addresses
-    Address address;
-    LatLng latLng;
-    Marker marker;
-    Circle circle;
+    GoogleMap mMap;
 
+    /**
+     * Sets up the spinner to hold the colors
+     */
+    private void initializeSpinner() {
+        spinner = findViewById(R.id.color_input); // For selecting colors
+        spinner.setAdapter(
+                new ArrayAdapter<>(
+                        this,
+                        androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                        new String[]{"red", "blue", "green", "yellow", "magenta", "cyan", "white"}
+                )
+        );
+        spinner.setVisibility(View.GONE);
+    }
 
-    int[] colors = new int[]{Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW,Color.MAGENTA, Color.CYAN, Color.WHITE}; // Change this to all possible color enums
-    Collection<Circle> addresses = new HashSet<>();
-    Collection<Circle> newAddresses = new HashSet<>();
-    Collection<Circle> removedAddresses = new HashSet<>();
-
-
+    /**
+     * Gets all variables from the activity and matches them to the local variables
+     */
     private void initializeVariables() {
+        setContentView(R.layout.activity_fill_map);
+
+        mapNameTextView = findViewById(R.id.top_text_view);
+        mapNameTextView.setText(getIntent().getStringExtra(ToMap.MAP_NAME));
+
         errorTextView = findViewById(R.id.error_text_view); // For displaying error messages to the user
         addressTextView = findViewById(R.id.address_input); // For entering addresses to add to the map
-        spinner = findViewById(R.id.color_input); // For selecting colors
-        arrayAdapter = new ArrayAdapter<>(
-                this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
-                new String[]{"red", "blue", "green", "yellow", "magenta", "cyan", "white"}); // Fills the spinner
+
+        initializeSpinner();
 
         fragment = (FillMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
         mMap = fragment.getmMap();
         mMap.setOnCircleClickListener(this::onCircleClick);
-        geocoder = new Geocoder(getApplicationContext());
 
-        btnEnterSubmit = findViewById(R.id.enter_submit_button);
-        btnFinishCancel = findViewById(R.id.finish_cancel_button);
+        btnEnterSubmitDelete = findViewById(R.id.enter_submit_delete_button);
+        btnCancelSave = findViewById(R.id.cancel_save_button);
 
-        btnEnterSubmit.setOnClickListener(this::EnterSubmit);
-        btnFinishCancel.setOnClickListener(this::FinishCancel);
-
-        spinner.setAdapter(arrayAdapter);
-        spinner.setVisibility(View.GONE);
+        btnEnterSubmitDelete.setOnClickListener(this::EnterSubmitDelete);
+        btnCancelSave.setOnClickListener(this::CancelSave);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_fill_map);
+        fillMapBackend = new FillMapBackend(
+                this,
+                getIntent().getIntExtra(ToMap.MAP_ID, -1)
+        );
 
         // Initialize all variables by finding them from the view
         initializeVariables();
-
-        // Get the Intent that started this activity and extract the string
-        String mapName = getIntent().getStringExtra(ToMap.MAP_NAME);
-        mapID = getIntent().getIntExtra(ToMap.MAP_ID, -1);
-        // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.top_text_view);
-        textView.setText(mapName);
     }
 
+    /**
+     * Handles what happens when a circle on the map is clicked.
+     * Selects the circle parameter and allows to delete it
+     * @param circle - the circle to be deleted
+     */
     public void onCircleClick(Circle circle) {
-        latLng = circle.getCenter();
-        marker = fragment.addMarker(latLng); // Places marker for feedback to user
+        fillMapBackend.onCircleClick(circle);
         swapToDeleteAddress();
     }
 
-    private void Enter(View view) {
-        // Pressing the enter button
-        try {
-            // Try to get an Address from the text in the addressTextView
-            address = geocoder.getFromLocationName(addressTextView.getText().toString(), 1).get(0); // Fails if there is no result
-        } catch (Exception e) {
-            // Show error to user and abort
-            errorTextView.setText(R.string.error_finding_address);
-            swapToEnterAddress();
-            return;
-        }
-        latLng = new LatLng(address.getLatitude(), address.getLongitude());
-        marker = fragment.addMarker(latLng); // Places marker for feedback to user
-        errorTextView.clearComposingText();
-    }
-
-    private void Submit(View view) {
-        // Pressing the submit button
-        marker.remove(); // Temporary marker gets removed
-        final int colorIndex = spinner.getSelectedItemPosition();
-
-        Circle circle = fragment.addCircle(latLng, colors[colorIndex]);
-
-        addresses.add(circle);
-        newAddresses.add(circle);
-    }
-
-    private void Finish(View view) {
-        // Pressing the finish button
-
-        Connection connection = null;
-        connection = ConnectionHelp.connect(connection, this);
-
-        for (Circle c : newAddresses) {
-            final int colorID = GetMap.getColorID(c.getFillColor(), connection);
-            final int addressID = GetMap.getAddressIDSafe(c.getCenter().latitude, c.getCenter().longitude, connection);
-
-            // Insert entry into database
-            GetMap.updateAddressSafe(addressID, mapID, colorID, connection);
-        }
-
-        for (Circle c : removedAddresses) {
-            final int colorID = GetMap.getColorID(c.getFillColor(), connection);
-            final int addressID = GetMap.getAddressIDSafe(c.getCenter().latitude, c.getCenter().longitude, connection);
-
-            // Remove entry from database
-            GetMap.removeAddress(addressID, mapID, colorID, connection);
-        }
-
-        ConnectionHelp.closeConnection(connection);
-
-        // Return to home screen
-        startActivity(new Intent(this, MainActivity.class));
-    }
-
-    private void Cancel(View view) {
-        // Pressing the cancel button
-        marker.remove(); // Remove temporary marker
-    }
-
-    private void Delete(View view) {
-        // Pressing the delete button
-        addresses.remove(circle);
-        newAddresses.remove(circle);
-        removedAddresses.add(circle);
-    }
-
+    /**
+     * Shows the addressTextView, Enter button and Save button.
+     * Hides other components
+     */
     private void swapToEnterAddress() {
-        // Shows the addressTextView, Enter button and Finish button
-        // Hides other components
-        btnEnterSubmit.setText(R.string.enter);
-        btnFinishCancel.setText(R.string.finish);
+        btnEnterSubmitDelete.setText(R.string.enter);
+        btnCancelSave.setText(R.string.save);
         spinner.setVisibility(View.GONE);
         addressTextView.setVisibility(View.VISIBLE);
     }
 
+    /**
+     * Shows the spinner, Submit button and Cancel button.
+     * Hides other components
+     */
     private void swapToSubmitAddress() {
-        // Shows the spinner, Submit button and Cancel button
-        // Hides other components
-        btnEnterSubmit.setText(R.string.submit);
-        btnFinishCancel.setText(R.string.cancel);
+        btnEnterSubmitDelete.setText(R.string.submit);
+        btnCancelSave.setText(R.string.cancel);
         spinner.setVisibility(View.VISIBLE);
         addressTextView.setVisibility(View.GONE);
     }
 
+    /**
+     * Shows the Delete button and Cancel button.
+     * Hides other components
+     */
     private void swapToDeleteAddress() {
-        // Shows the Delete button and Cancel button
-        // Hides other components
-        btnEnterSubmit.setText(R.string.delete);
-        btnFinishCancel.setText(R.string.cancel);
+
+        btnEnterSubmitDelete.setText(R.string.delete);
+        btnCancelSave.setText(R.string.cancel);
         spinner.setVisibility(View.GONE);
         addressTextView.setVisibility(View.GONE);
     }
 
-    private void EnterSubmit(View view) {
-        if (btnEnterSubmit.getText().equals(getString(R.string.enter))) {
-            swapToSubmitAddress(); // Toggle button text
-            Enter(view);
+    /**
+     * Handles the click for the buttons Cancel and Save.
+     * Updates button texts
+     * @param view the current view
+     */
+    private void CancelSave(View view) {
+        swapToEnterAddress();
+        if (btnCancelSave.getText().equals(getString(R.string.cancel))) {
+            fillMapBackend.Cancel();
         } else {
-            swapToEnterAddress(); // Toggle button text
-            Submit(view);
+            fillMapBackend.Save();
+            // Return to home screen
+            startActivity(new Intent(this, MainActivity.class));
         }
     }
 
-    private void FinishCancel(View view) {
-        if (btnFinishCancel.getText().equals(getString(R.string.finish))) {
-            Finish(view);
-        } else if (btnFinishCancel.getText().equals(getString(R.string.delete))) {
-            Delete(view);
-            swapToEnterAddress();
+    /**
+     * Handles the click for the buttons Enter, Submit and Delete.
+     * Updates button texts
+     * @param view the current view
+     */
+    private void EnterSubmitDelete(View view) {
+        if (btnEnterSubmitDelete.getText().equals(getString(R.string.enter))) {
+            swapToSubmitAddress();
         } else {
-            swapToEnterAddress(); // Toggle button text
-            Cancel(view);
+            swapToEnterAddress();
         }
+
+        if (btnEnterSubmitDelete.getText().equals(getString(R.string.enter))) {
+            String searchString = addressTextView.getText().toString();
+            if (!fillMapBackend.Enter(searchString))
+                swapToEnterAddress(); // Search not succeeded
+        } else if (btnEnterSubmitDelete.getText().equals(getString(R.string.submit))) {
+            fillMapBackend.Submit();
+        } else {
+            fillMapBackend.Delete();
+        }
+    }
+
+    public int getSpinnerColorIndex() {
+        return spinner.getSelectedItemPosition();
+    }
+
+    public Marker addMarker(LatLng latLng) {
+        return fragment.addMarker(latLng);
+    }
+
+    public Circle addCircle(LatLng latLng, int color) {
+        return fragment.addCircle(latLng, color);
+    }
+
+    public void setErrorText(int resid) {
+        errorTextView.setText(resid);
+    }
+
+    public void clearErrorText() {
+        errorTextView.clearComposingText();
     }
 }
